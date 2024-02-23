64ed6e5d929cee2f14f6a4b83eb2a21886faf95a
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-24 02:06:06 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: [
+			"http://localhost:5173",
+			"https://iinnsstagramclone.netlify.app/",
+		],
 		credentials: true,
 	})
 );
