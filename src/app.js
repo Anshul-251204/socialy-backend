45b609ad0edd9cb45e8dc06076cc06aaab7ca6a9
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-24 16:10:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-		credentials: true,
-	})
+	cors( 
+		//{
+		//origin: process.env.CORS_ORIGIN,
+		//credentials: true,
+	        //}
+	)
 );
 
 app.use(express.json({ limit: "16kb" }));
