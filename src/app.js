a4eb8b96b89d61a706de5c8f0eb3ee646fa4ead9
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-24 19:15:18 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -3,22 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(
-	cors( 
-		//{
-		//origin: process.env.CORS_ORIGIN,
-		//credentials: true,
-	        //}
-	)
-);
-
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
-
-import userRouter  from "./routes/user.routes.js";
+import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
 import commentRouter from "./routes/comments.routes.js";
 import likeRouter from "./routes/like.routes.js";
@@ -28,19 +19,19 @@ import messageRouter from "./routes/message.routes.js";
 import conversationRouter from "./routes/conversation.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
-app.get("/",(req,res)=>{
-	res.send("working fine")
-})
+app.get("/", (req, res) => {
+	res.send("working fine");
+});
 
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/posts",postRouter);
+app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/comments", commentRouter);
-app.use("/api/v1/likes",likeRouter);
-app.use("/api/v1/stories",storyRouter);
-app.use("/api/v1/follows",followRouter);
-app.use("/api/v1/messages",messageRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/stories", storyRouter);
+app.use("/api/v1/follows", followRouter);
+app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/conversations", conversationRouter);
 
 export default app;
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
