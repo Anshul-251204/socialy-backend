4c521300b0757305ba51aa263a9b3765914ab8d3
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-26 02:29:58 +0530

    add cors origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-	cors()
+	cors({
+		origin: ["*", "http://localhost:5173/"],
+		credentials:true
+	})
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
