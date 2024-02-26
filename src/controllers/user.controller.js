1ac7e0979112c13dfa7c72112e7c4ae588286b90
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-26 23:28:49 +0530

    change cookies option

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -64,9 +64,10 @@ const registerUser = asyncHandler(async (req, res, next) => {
 	};
 
 	const optionsForResfresh = {
-		httpOnly: true,
-		secure: true,
-		expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+		 httpOnly:true,
+       		 secure:true,
+        	 sameSite:"none"
+		 expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
 	};
 
 	const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
@@ -120,8 +121,9 @@ const loginUser = asyncHandler(async (req, res, next) => {
 	);
 
 	const optionsForAccess = {
-		httpOnly: true,
-		secure: true,
+		httpOnly:true,
+        	secure:true,
+       		sameSite:"none"
 		expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
 	};
 
